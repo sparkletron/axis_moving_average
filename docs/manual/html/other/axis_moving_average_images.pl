669203ec6f9983a84b4736de00099b36f435e8e1
Jay Convertino <johnathan.convertino.1@us.af.mil>
2025-04-25 10:40:24 -0400

    update docs with math

--- docs/manual/html/other/axis_moving_average_images.pl
+++ docs/manual/html/other/axis_moving_average_images.pl
@@ -1,20 +1,20 @@
-# LaTeX2HTML 2021.2 (Released July 1, 2021)
+# LaTeX2HTML 2024 (Released January 1, 2024)
 # Associate images original text with physical files.
 
 
 $key = q/;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- STYLE="height: 0.46ex; vertical-align: -0.12ex; " SRC="|."$dir".q|axis_moving_average_img3.svg"
+ STYLE="height: 0.23ex; vertical-align: -0.12ex; " SRC="|."$dir".q|axis_moving_average_img3.svg"
  ALT="$ $">|; 
 
-$key = q/displaystylefrac{X_{0}+...X_{n}}{n};MSF=1.6;AAT/;
+$key = q/displaystyleSMA=sum_{i=0}^{log_2n}frac{D_0+dots+D_n}{n};MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- STYLE="height: 5.20ex; vertical-align: -1.71ex; " SRC="|."$dir".q|axis_moving_average_img2.svg"
- ALT="$\displaystyle \frac{X_{0} + ... X_{n}}{n}$">|; 
+ STYLE="height: 7.67ex; vertical-align: -3.09ex; " SRC="|."$dir".q|axis_moving_average_img2.svg"
+ ALT="$\displaystyle SMA = \sum_{i=0}^{\log_2 n}\frac{D_0+\dots+D_n}{n}$">|; 
 
 $key = q/includegraphics[width=0.90textwidth,height=textheight,keepaspectratio]{imgslashAFRL.png};AAT/;
 $cached_env_img{$key} = q|<IMG
- STYLE="height: 16.09ex; vertical-align: -0.12ex; " SRC="|."$dir".q|axis_moving_average_img1.svg"
+ STYLE="height: 15.85ex; vertical-align: -0.12ex; " SRC="|."$dir".q|axis_moving_average_img1.svg"
  ALT="\includegraphics[width=0.90\textwidth,height=\textheight,keepaspectratio]{img/AFRL.png}">|; 
 
 1;
